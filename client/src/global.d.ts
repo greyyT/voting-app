e63addf321d1b52d60e554392c5cde4e0814a013
greyyT <nhannguyen8338@gmail.com>
2023-10-18 19:16:53 +0700

    Setup redis through docker and commands

---
+++ client/src/global.d.ts
@@ -0,0 +1,1 @@
+type DeepReadonly<T> = T extends ( ...args: unknown[]) => unknown ? T : { readonly [P in keyof T]: DeepReadonly<T[P]> }
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('Main (main.ts)');
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: [
+        'https://localhost:8080',
+        /^http:\/\/192\.168\.1\.([1-9]|[1-9]\d):8080$/,
+      ],
+    },
+  });
+
+  const configService = app.get(ConfigService);
+  const port = parseInt(configService.get('PORT'));
+  await app.listen(port);
+
+  logger.log(`Server running on port ${port}`);
 }
 bootstrap();
